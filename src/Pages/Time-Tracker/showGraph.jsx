e757f4026eb99f74d3ae0890fc006cd051066f7d
R-S-A-N-J-A-Y <sanjay638291@gmail.com>
2024-07-15 19:33:52 +0530

    testing

---
+++ src/Pages/Time-Tracker/showGraph.jsx
@@ -0,0 +1,5 @@
+const showGraph1 = () => {
+    return (<h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate assumenda, eum laboriosam consequatur recusandae totam minus non laborum ipsum debitis ab placeat id dolorem cum soluta exercitationem illo modi quam.</h1>)
+}
+
+export default showGraph1;
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Editor from './Pages/Editor/index.jsx';
+import showGraph1 from './Pages/Time-Tracker/showGraph.jsx';
 
 import './CSS File/general.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Editor />
+    <>
+        <Editor />
+        <showGraph />
+    </>
 );
