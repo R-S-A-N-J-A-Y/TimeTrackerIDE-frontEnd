cdd0520875fb340a3c4dc96fe89e18af880b1b24
R-S-A-N-J-A-Y <sanjay638291@gmail.com>
2024-09-22 16:08:18 +0530

    Publishing the React Page

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import Editor from './Pages/index.jsx';
 
-import './CSS File/general.css'
-import './CSS File/Editor.css'
-import './CSS File/Notations.css'
+import './CSS File/general.css';
+import './CSS File/Editor.css';
+import './CSS File/Notations.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <>
+    <BrowserRouter basename="/TimeTracker-IDE">
         <Editor />
-    </>
+    </BrowserRouter>
 );
